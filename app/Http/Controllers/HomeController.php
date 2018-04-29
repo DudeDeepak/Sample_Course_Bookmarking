12aa49889ca14dde0f006822cd6878c0422fe0a8
Deepak <deepakpalakkal2795@gmail.com>
2018-04-29 21:12:49 +0530

    Controller : Added functionality to inser data into the bookmark table
    This is basically done to view the bookmarked course on another page
    This functionality would enable the insertion of data based in AJAX call
    and sends back the required response status in JSON format
    
    Signed-off-by: Deepak <deepakpalakkal2795@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use App\Bookmark;
 
 class HomeController extends Controller
 {
@@ -21,5 +22,44 @@ class HomeController extends Controller
                 return view('home', compact('arr'));
         }
 
+	// Function to insert the course data into bookmarks table
+        // and pass the response to ajax call from view  in json format
+	public function actionInsert(Request $request) {
+
+                // $someInput = $request->all();
+
+                try {
+
+                $this->validate($request, array(
+                        'course_id' => 'required',
+                        'course_name' => 'required',
+                        'course_type' => 'required',
+                        ));
+
+                $bookmark = new Bookmark();
+
+                $bookmark->course_id = $request->input('course_id');
+                $bookmark->course_name = $request->input('course_name');
+                $bookmark->course_type = $request->input('course_type');
+
+                $sample_array = $bookmark->where('course_id', $request->input('course_id'))->get()->toArray();
+
+                if(!empty($sample_array))
+                        $response_array['msg'] = 'already-exist';
+                else {
+                        $bookmark->save();
+                        $response_array['msg'] = 'success';
+                     }
+
+                } catch(\Exception $e) {
+                        $response_array['msg'] = 'error';
+                        header('Content-type: application/json');
+                        echo json_encode($response_array);
+                }
+
+                header('Content-type: application/json');
+                echo json_encode($response_array);
+
+	}
 
 }

22a3da2c740008b4162dc862db938e628ba9c584
Deepak <deepakpalakkal2795@gmail.com>
2018-04-29 21:34:03 +0530

    Controller : Added controller function to pass data to the bookmark page
    This is required so that we can separately view the bookmarked list
    in another page .
    
    Signed-off-by: Deepak <deepakpalakkal2795@gmail.com>

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Bookmark;
 
 class BookmarkController extends Controller
 {
-    //
+
+	//This function passes the stored bookmark to the bookmark page
+	public function index() {
+                $bookmarks = Bookmark::all();
+                $bookmarks = json_decode(json_encode($bookmarks), true);
+                return view('bookmark')->with('bookmarks', $bookmarks);
+
+        }
+
 }

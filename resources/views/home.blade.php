122175cb4edca2c6a83e54fb3160f59c64e217dc
Deepak <deepakpalakkal2795@gmail.com>
2018-04-29 20:47:11 +0530

    View : Added Growl library to show the notifications to the user
    Signed-off-by: Deepak <deepakpalakkal2795@gmail.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,10 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+<link href="http://ksylvest.github.io/jquery-growl/stylesheets/jquery.growl.css" rel="stylesheet" type="text/css">
+<script src='http://ksylvest.github.io/jquery-growl/javascripts/jquery.growl.js' type='text/javascript'></script>
+
+
 <style>
 
 #headding {

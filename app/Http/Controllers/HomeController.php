c1fc294ff39a1875506311aa1c5e10f28b429f10
Deepak <deepakpalakkal2795@gmail.com>
2018-04-29 19:52:14 +0530

    Controller : Added functionality to consume api and pass data to home page
    Basically Guzzle Http was used to consume the data using get
    request pass an array to home page to show course details
    
    Signed-off-by: Deepak <deepakpalakkal2795@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,8 +5,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class HomeController extends Controller
 {
-    //
+  	// index function con consume api and pass course details to home page 
+        public function index() {
+
+                $client = new Client(['base_uri' => 'https://api.coursera.org/']);
+                $response = $client->request('GET', 'api/courses.v1');
+                $body = $response->getBody();
+                $content =$body->getContents();
+                $arr = json_decode($content,TRUE);
+
+                return view('home', compact('arr'));
+        }
+
+
 }
